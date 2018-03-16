b75a02b3499e085137604356d7a60d75d6e70b45
Jordy Vieira <jordyvieira@hotmail.com>
2018-03-16 18:50:40 -0300

    change derivative method

--- newton.py
+++ newton.py
@@ -2,25 +2,25 @@ from math import *
 from sympy import diff
 import numpy as np
 
-def derivada(f, x, h):
-	d = (1.0/(2*h))*(f(x+h)-f(x-h))
-	return d
+def derivative(f, x, h):
+    d = (f(x+h)-f(x))/h
+    return d
 
 def newton(f, x0, h):
-	i = 0
-	lastX = x0
-	nextX = lastX + 10 * h
-	
-	while (abs(lastX - nextX) > h):
-		newY = f(nextX)
-		lastX = nextX
-		nextX = lastX - newY / derivada(f, lastX, h)
-		i += 1
-		print (i)
-		
-	return nextX
+    i = 0
+    lastX = x0
+    nextX = lastX + 10 * h
+
+    while (abs(lastX - nextX) > h):
+        newY = f(nextX)
+        lastX = nextX
+        nextX = lastX - newY / derivative(f, lastX, h)
+        i += 1
+        print (i)
+                
+    return nextX
 
 def func(x):
-	return np.exp(-x) - x
+    return np.exp(-x) - x
 
-print(newton(func, 5, 0.001))
+print(newton(func, 5, 0.00001))
